ce154e360f6ebef97f1c0a4f5eda520a45e3dab5
fsarraf <fnsarraf@gmail.com>
2019-11-01 12:53:32 +0000

    firday noon

--- lib.py
+++ lib.py
@@ -2,17 +2,36 @@
 import requests
 from dotenv import load_dotenv
 import os
+import pymongo
 
 load_dotenv()
 
 class MongoHandler():
-    pass
+    
+    def __init__(self, dbname,div):
+        self.myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
+        self.db = self.myclient[dbname]
+        self.collection= self.db[div]
+
+   
+   
+    
+    def create_doc(self, team_name, total_goals, total_wins, win_percentage):
+        dic={}
+        dic['Team_Name']= team_name 
+        dic['total_goals']= total_goals
+        dic['total wins']= total_wins
+        dic['win_percentage']= win_percentage
+        return self.collection.insert_one(dic)
+
 
 class WeatherGetter():
-    def __init__(self):
+    def __init__(self, time = None):
         self.BASE_URL = 'https://api.darksky.net/'
         self.SECRET_KEY = os.getenv("DARKSKY_KEY")
+        self.time = time
         
-    def weather_getter(self):
-        forcast = requests.get(f"{self.BASE_URL}forecast/{self.SECRET_KEY}/%s,%s")
+        
+    def weather_getter(self, time):
+        forcast = requests.get(f"{self.BASE_URL}forecast/{self.SECRET_KEY}/52.5200,13.4050,{time}?exclude=minutely,hourly,daily,alerts,flags")
         return forcast
